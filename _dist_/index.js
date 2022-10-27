68925fb08ce90f6355dcd05a8e0525fdd43ec4c2
MatiRivarola <MatiRivarola@users.noreply.github.com>
2022-10-27 13:03:24 +0000

    deploy: c896ad4dd7087415a1d2dfc88f36630702641686

---
+++ _dist_/index.js
@@ -0,0 +1,17 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+const url = "https://platzi-avo.vercel.app/api/avo"
+
+window
+    .fetch(url)
+
+    .then((respuesta) => respuesta.json())
+
+    .then((respuestaJson) => {
+        respuestaJson.data.forEach(item => {
+            console.log(item.name)
+        });
+    })
